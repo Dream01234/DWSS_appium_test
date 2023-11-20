758c59f9894e38bada6990e81d2d53bfd3b99b2b
Dr.Li <417081473@qq.com>
2023-11-20 14:20:38 +0800

    修改testcase login

--- dwss_tests/test_login.py
+++ dwss_tests/test_login.py
@@ -5,7 +5,7 @@ import dwss_appium_driver.log.logger
 log = dwss_appium_driver.log.logger()
 
 @pytest.mark.parametrize("username, password", [("kentestgrp10", "12345678")])
-def test_login(driver_setup, username, password):
+def test_login(username, password):
     # 进行登录操作
     log.info("-----------用例开始---------")
     a = dwss_Combinatorial.login_combinatorial.loginpage_control()
